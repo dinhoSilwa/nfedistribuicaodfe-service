2a39456bf3ffe260dffb0e01b9ab0ead4fd8865f
dinhoSiwa <claudiosilva.one@gmail.com>
2026-01-18 17:22:50 -0300

    update-branch

--- config.py
+++ config.py
@@ -2,15 +2,11 @@
 
 # Caminho relativo ou absoluto do certificado A1 (.pfx ou .p12)
 CERT_PFX = "certificado.pfx"  # Altere para o nome real do seu arquivo
-
 # Senha do certificado
 CERT_PASSWORD = "123456"  # Altere para a senha real
-
 # CNPJ/CPF vinculado ao certificado (deve ter acesso à nota)
 CNPJ_INTERESSADO = "34683891000140"
-
 # Chave de acesso da NFC-e/NF-e que deseja buscar
 CHAVE_DESEJADA = "23250834683891000140650220000207061299677609"
-
 # Nome da pasta (dentro do diretório do script) onde os XMLs serão salvos
 PASTA_XML = "xmls"
